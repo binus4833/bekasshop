e1ae7a4f198465a03386de159d92c43b2a61c932
CalvinDjaja <73376317+CalvinDjaja@users.noreply.github.com>
2020-12-04 00:56:51 +0700

    Add files via upload
    Java OrderDetailActivity

---
+++ bekasshop_application/app/src/main/java/com/example/bekasshop/Activity/OrderActivity.java
@@ -0,0 +1,85 @@
+package com.example.bekasshop.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bekasshop.R;
+import com.squareup.picasso.Picasso;
+
+
+public class OrderActivity extends AppCompatActivity {
+
+    ImageView itemImg;
+    TextView itemName, itemPrice;
+    EditText itemQty;
+    Button orderBtn;
+    int resId, jmlQty;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+        Intent intent = getIntent();
+
+        itemImg = findViewById(R.id.imageView);
+        itemName = findViewById(R.id.nameItem);
+        itemPrice = findViewById(R.id.priceItem);
+        itemQty = findViewById(R.id.editTextQuantity);
+        orderBtn = findViewById(R.id.btnOrder);
+
+        itemName.setText(intent.getExtras().getString("itemName"));
+        Picasso.get()
+                .load("http://192.168.1.5/Latihan/"+ intent.getExtras().getString("itemImage"))
+                .into(itemImg);
+        final int price = intent.getExtras().getInt("itemPrice");
+        itemPrice.setText("Rp. " + price);
+
+        orderBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(itemQty.getText().toString()) || Integer.parseInt(itemQty.getText().toString()) < 1){
+                    Toast.makeText(getApplicationContext(), "Empty field and 0 not allowed!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+
+                }
+            }
+        });
+        ImageView img = findViewById(R.id.myOrder);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+
+    }
+
+    public void plus(View view){
+        jmlQty = jmlQty + 1;
+        text(jmlQty);
+    }
+
+    public void minus(View view){
+        if (jmlQty > 0){
+            jmlQty = jmlQty - 1;
+            text(jmlQty);
+        }
+    }
+
+    private void text(int jumlah){
+        itemQty = findViewById(R.id.editTextQuantity);
+        itemQty.setText("" + jumlah);
+    }
+}
