043de25ebc6668dadec1953fc892e30394cd6454
CalvinDjaja <73376317+CalvinDjaja@users.noreply.github.com>
2020-12-05 22:50:31 +0700

    Add files via upload
    Database for this application

---
+++ bekasshopAPI/database.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `bekasshopdb`.`users` ( `id` INT NOT NULL AUTO_INCREMENT , `emails` 
+VARCHAR(100) NOT NULL , `username` VARCHAR(100) NOT NULL , `password` VARCHAR(100) 
+NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+INSERT INTO `products` (`id`, `name`, `description`, `image`, `price`, `category`) VALUES 
+(NULL, 'Yoyo', 'Yoyo Bekas pakai oleh juara 1', 'Yoyo_Bekas.jpg', '100000', 'toys'),
+(NULL, 'Kuda Jumping', 'kuda jumping yang sudah diapaki!', 'Kuda_Jumping.jpg', '100000', 'toys'),
+(NULL, 'Yoyo', 'Yoyo bekas yang sudah diapaki!', 'Yoyo_Bekas.jpg', '100000', 'toys'),
+(NULL, 'Yoyo', 'Yoyo bekas yang sudah diapaki!', 'Yoyo_Bekas.jpg', '100000', 'toys'),
+(NULL, 'Yoyo', 'Yoyo bekas yang sudah diapaki!', 'Yoyo_Bekas.jpg', '100000', 'toys'),
+(NULL, 'Yoyo', 'Yoyo bekas yang sudah diapaki!', 'Yoyo_Bekas.jpg', '100000', 'toys'),
+(NULL, 'Yoyo', 'Yoyo bekas yang sudah diapaki!', 'Yoyo_Bekas.jpg', '100000', 'toys'),
+(NULL, 'Boneka', 'boneka buatan andi', 'Boneka.jpg', '20000', 'toys')
+
