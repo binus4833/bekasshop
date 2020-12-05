8361652a229a8ffe6b2d528b53ee0680b2cbb41a
NicholasDjaja <73376295+NicholasDjaja@users.noreply.github.com>
2020-12-05 23:32:03 +0700

    Update database.sql
    insert data

--- bekasshopAPI/database.sql
+++ bekasshopAPI/database.sql
@@ -15,5 +15,17 @@ INSERT INTO `products` (`id`, `name`, `description`, `image`, `price`, `category
 (NULL, 'Mobil AVV Second', 'Mobil AVV yang sudah diapaki!', 'Mobil_AVV_Bekas.jpg', '50000000', 'automotive'),
 (NULL, 'Ban Bekas', 'Ban yang sudah dipake bertahun - tahun!', 'Ban_Bekas.jpg', '50000', 'automotive'),
 (NULL, 'Spion Tiger Motor', 'Spion Tiger Motor yang sudah diapaki!', 'Spion_Tiger_Motor.jpg', '30000', 'automotive'),
-(NULL, 'Spion Mobil', 'Spion Mobil yang sudah diapaki!', 'Spion_Mobil.jpg', '1000000', 'automotive')
+(NULL, 'Spion Mobil', 'Spion Mobil yang sudah diapaki!', 'Spion_Mobil.jpg', '1000000', 'automotive'),
+(NULL, 'TV LG Second', 'TV LG Second Kondisi 9/10, bagian belakang sedikit berdebu', 'TV_LG.jpg', '2500000', 'electronic'),
+(NULL, 'Printer Epson L3100', 'Printer Second Epson L3100 kondisi baru, pakai 5 bulan kantor', 'epson.jpg', '3500000', 'electronic'),
+(NULL, 'Laptop Asus ROG Strix 3', 'Laptop ROG Strix 3 kondisi bagus tombol enter sedikit rusak', 'Asus_ROG.jpg', '12000000', 'electronic'),
+(NULL, 'Headseat Steelseries II', 'Headseat Steelseries II kondisi baik baru dipakai 2 bulan', 'steelseries.jpg', '2000000', 'electronic'),
+(NULL, 'Mouse Logitech G502 Hero', 'Mouse Logitech G502 baru pakai 2 minggu, diganti karena tidak cocok', 'g502.jpg', '600000', 'electronic'),
+(NULL, 'Mouse Logitech G102 Light', 'Mouse Logitech G102 garasi 1 tahun 5 febuari', 'g102.jpg', '200000', 'electronic'),
+(NULL, 'Laptop Asus zenbook', 'Laptop Asus dijual karena dibutuhkan laptop yang lebih baik', 'zenbook.jpg', '9000000', 'electronic'),
+(NULL, 'Komputer Rakit RTX 2080', 'Dijual cepat komputer RTX 2080', 'komputer.jpg', '21000000', 'electronic'),
+(NULL, 'Kursi Gaming Omega', 'Kursi Gaming Omega kondisi 10/10 box ready', 'kursi_omega.jpg', '5000000', 'furniture'),
+(NULL, 'Sofa KINGKOIL', 'Sofa Kingkoil dijual karena ingin pindah rumah', 'sofaking.jpg', '12000000', 'furniture'),
+(NULL, 'Meja makan informa', 'Meja makan informa ukuran untuk 6 orang kondisi 8/10', 'meja_makan.jpg', '6000000', 'furniture'),
+(NULL, 'Kursi makan 4 Set', 'Kursi makan dijual 4 pcs harga 4pcs', 'kursi4pcs.jpg', '2400000', 'furniture')                        
 
